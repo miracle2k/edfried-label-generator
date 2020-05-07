82a2dd5687e1567a6b4685deb30c68fcea695bac
Michael Elsdörfer <michael@elsdoerfer.info>
2020-05-07 10:51:11 +0100

    Add sentry.

--- App.js
+++ App.js
@@ -1,4 +1,11 @@
 import React from 'react';
 import { App } from './src';
 
+import * as Sentry from '@sentry/react-native';
+
+Sentry.init({ 
+  dsn: 'https://898b2c40cf4442b9abc6e959241e49e3@sentry2.s.dpool.org/29', 
+});
+
+
 export default App;
--- android/app/src/main/java/com/product_label_generator/MainApplication.java
+++ android/app/src/main/java/com/product_label_generator/MainApplication.java
@@ -3,6 +3,7 @@ package com.product_label_generator;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.sentry.RNSentryPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSentryPackage(),
             new RNFetchBlobPackage(),
             new RNGestureHandlerPackage(),
             new ReactNativeDocumentPicker(),
